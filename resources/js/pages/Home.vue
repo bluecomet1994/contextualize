4d8c56ba1e0c1c3e56826bf33afe5c68dd5515e5
Morgan Mastrangelo <morgan172cm@gmail.com>
2023-11-05 20:02:52 -0500

    set project

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1 class="text-3xl">Hello world</h1>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+    console.log('Home page loaded');
+});
+</script>
